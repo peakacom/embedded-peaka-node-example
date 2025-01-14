f2cabd460d337887035c169affbd18357a21c302
Ozan Batuhan Ceylan <ozan@code2.io>
2025-01-14 16:59:01 +0300

    fix

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,60 +1,90 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+#!/usr/bin/env node
 
-var indexRouter = require('./routes/index');
-const cors = require('cors');
-require('dotenv').config()
+/**
+ * Module dependencies.
+ */
 
-const corsOptions = {
-  origin: 'https://embedded-peaka-node-example-5p71.vercel.app',
-  optionsSuccessStatus: 200,
-};
+var app = require('../app');
+var debug = require('debug')('backend:server');
+var http = require('http');
 
-var app = express();
+/**
+ * Get port from environment and store in Express.
+ */
 
-app.use(cors(corsOptions));
+var port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/**
+ * Create HTTP server.
+ */
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  console.log("dynamic")
-  const dynamicDomain = req.headers['origin'] || 'https://defaultdomain.com';
-  const cspHeader = `default-src 'self'; script-src 'self' ${dynamicDomain}; frame-ancestors 'self';`.trim();
-  
-  res.setHeader('Content-Security-Policy', cspHeader);
-  
-  next();
-});
+var server = http.createServer(app);
 
-app.use('/', indexRouter);
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
+/**
+ * Normalize a port into a number, string, or false.
+ */
 
+function normalizePort(val) {
+  var port = parseInt(val, 10);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.send(JSON.stringify(err));
-});
+  return false;
+}
 
-module.exports = app;
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
---
+++ backend/app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./api/routes/index');
+const cors = require('cors');
+require('dotenv').config()
+
+const corsOptions = {
+  origin: 'https://embedded-peaka-node-example-5p71.vercel.app',
+  optionsSuccessStatus: 200,
+};
+
+var app = express();
+
+app.use(cors(corsOptions));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  console.log("dynamic")
+  const dynamicDomain = req.headers['origin'] || 'https://defaultdomain.com';
+  const cspHeader = `default-src 'self'; script-src 'self' ${dynamicDomain}; frame-ancestors 'self';`.trim();
+  
+  res.setHeader('Content-Security-Policy', cspHeader);
+  
+  next();
+});
+
+app.use('/', indexRouter);
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(JSON.stringify(err));
+});
+
+module.exports = app;
