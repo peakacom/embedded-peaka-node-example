5b3691bf40600454ece22d442ff93722ce324e22
Ozan Batuhan Ceylan <ozan@code2.io>
2025-10-31 11:22:30 +0300

    Refactor backend

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./api/routes/index');
+var indexRouter = require('./routes/index');
 const cors = require('cors');
 require('dotenv').config()
 
